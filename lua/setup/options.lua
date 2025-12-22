fd2dc66fca8e5afe825e1bf1267ef8bb76be646c
Perry280 <mason.alessio.am@gmail.com>
2025-12-22 19:46:08 +0100

    c_syntax_for_h in options.lua

--- lua/setup/options.lua
+++ lua/setup/options.lua
@@ -34,5 +34,7 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
+vim.g.c_syntax_for_h = true
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
